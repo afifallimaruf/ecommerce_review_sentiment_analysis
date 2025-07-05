6cab901d3188d05f0d30984892ab2856cb8173aa
afifallimaruf <afifalli18maruf@gmail.com>
2025-07-05 16:53:40 +0700

    menghapus fungsi regex untuk hapus alamat email, pertahankan alphanumeric, spasi dan tanda baca dasar

--- ML/scripts/data_loader.py
+++ ML/scripts/data_loader.py
@@ -150,15 +150,6 @@ class DataLoader:
         # Hapus URLs
         text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
 
-        # Hapus alamat email
-        text = re.sub(r'\S+@\S+', '', text)
-
-        # Pertahankan alphanumeric, spasi, tanda baca dasar
-        text = re.sub(r'[^a-zA-Z0-9\s\.\!\?\,\;\-\'\"]', '', text)
-
-        # Hapus ekstra spasi
-        text = re.sub(r'\s+', ' ', text).strip()
-
         return text
 
     def analyze_dataset(self, df: Optional[pd.DataFrame] = None) -> Dict:
