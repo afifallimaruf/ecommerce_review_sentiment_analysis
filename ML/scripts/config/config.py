d0ca81dc94e0bf4671600ccbaa2141ec9c141c0f
afifallimaruf <afifalli18maruf@gmail.com>
2025-08-06 13:19:02 +0700

    mengubah file config

--- ML/scripts/config/config.py
+++ ML/scripts/config/config.py
@@ -9,16 +9,17 @@ Date:2025
 import json
 from typing import Dict
 from log.logging import SetupLogging
+from pathlib import Path
 
 
 logger_init = SetupLogging()
 logger = logger_init.set_logger()
 
-
 class LoadConfig:
     """ Class untuk load config.json """
-    def __init__(self, config_path: str="config.json"):
-        self.config_path = config_path
+    def __init__(self, config_name="config.json"):
+        root_path = Path(__file__).resolve().parent.parent.parent
+        self.config_path = root_path / config_name
 
     def load_config(self) -> Dict:
         """Load konfigurasi dari JSON file"""
@@ -27,4 +28,3 @@ class LoadConfig:
                 return json.load(file)
         except Exception as e:
             logger.error(f"Error when load config file: {e}")
-            
